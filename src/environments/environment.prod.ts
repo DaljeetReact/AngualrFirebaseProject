61388e00e094ac1f94931eceae8973c0fada5247
Singh <daljeet.test99@gmail.com>
2022-05-10 09:58:45 +0200

    install firebase  with  angular cli make new module build all module together

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'angularfireship-a4249',
+    appId: '1:62970759727:web:77155a3965b7204b1d94da',
+    storageBucket: 'angularfireship-a4249.appspot.com',
+    apiKey: 'AIzaSyCFnGOnYYE0ZQ5ecDdDl0u66h-niNucLC8',
+    authDomain: 'angularfireship-a4249.firebaseapp.com',
+    messagingSenderId: '62970759727',
+    measurementId: 'G-GQEY4VY6WV',
+  },
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCFnGOnYYE0ZQ5ecDdDl0u66h-niNucLC8",
+    authDomain: "angularfireship-a4249.firebaseapp.com",
+    projectId: "angularfireship-a4249",
+    storageBucket: "angularfireship-a4249.appspot.com",
+    messagingSenderId: "62970759727",
+    appId: "1:62970759727:web:77155a3965b7204b1d94da",
+    measurementId: "G-GQEY4VY6WV"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,25 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'angularfireship-a4249',
+    appId: '1:62970759727:web:77155a3965b7204b1d94da',
+    storageBucket: 'angularfireship-a4249.appspot.com',
+    apiKey: 'AIzaSyCFnGOnYYE0ZQ5ecDdDl0u66h-niNucLC8',
+    authDomain: 'angularfireship-a4249.firebaseapp.com',
+    messagingSenderId: '62970759727',
+    measurementId: 'G-GQEY4VY6WV',
+  },
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCFnGOnYYE0ZQ5ecDdDl0u66h-niNucLC8",
+    authDomain: "angularfireship-a4249.firebaseapp.com",
+    projectId: "angularfireship-a4249",
+    storageBucket: "angularfireship-a4249.appspot.com",
+    messagingSenderId: "62970759727",
+    appId: "1:62970759727:web:77155a3965b7204b1d94da",
+    measurementId: "G-GQEY4VY6WV"
+  }
 };
 
 /*
