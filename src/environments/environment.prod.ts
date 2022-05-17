f4689e1ad42e924315340b478e82863d2fde158a
Singh <daljeet.test99@gmail.com>
2022-05-17 12:00:05 +0200

    done with email  and google login with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
-  firebase: {
-    apiKey: "AIzaSyCFnGOnYYE0ZQ5ecDdDl0u66h-niNucLC8",
-    authDomain: "angularfireship-a4249.firebaseapp.com",
-    databaseURL: "https://angularfireship-a4249-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "angularfireship-a4249",
-    storageBucket: "angularfireship-a4249.appspot.com",
-    messagingSenderId: "62970759727",
-    appId: "1:62970759727:web:77155a3965b7204b1d94da",
-    measurementId: "G-GQEY4VY6WV"
-  },
+
   production: true
 };
