a0e3a67a4b17b49286e4d30a89abaf5d82865543
Singh <daljeet.test99@gmail.com>
2022-05-18 10:08:24 +0200

    add redirect to login page is user not logged in

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Routes, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-
+import {Router} from '@angular/router';
 import { SnackService } from '../services/snack.service';
 @Injectable({
   providedIn: 'root'
@@ -11,12 +11,13 @@ export class AuthGuard implements CanActivate {
   
   constructor(
     private afauth: AngularFireAuth,
-    private snack:SnackService
+    private snack:SnackService,
+    private route:Router
     ){}
 
   async canActivate(
      next: ActivatedRouteSnapshot,
-     state: RouterStateSnapshot
+     state: RouterStateSnapshot,
     ): Promise<boolean>{
       const user = await this.afauth.currentUser;
       const isLogin =   !!user;
@@ -24,6 +25,7 @@ export class AuthGuard implements CanActivate {
       console.log(typeof(isLogin)+isLogin);
       if(!isLogin){
         this.snack.authError();
+        this.route.navigate(['/login']);
       }
       return isLogin;
   }
